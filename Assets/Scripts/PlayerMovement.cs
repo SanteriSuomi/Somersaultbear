89a9daf7d0263e97e272cc13491e32f5b401868d
SanteriSuomi <sant.suomi@gmail.com>
2019-09-10 16:25:00 +0300

    Initial commit (small prototype)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Require rigidbody for movement
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+	// Rigidbody field
+	private Rigidbody2D rigidBody;
+
+	// Vertical movement speed modifier
+	[SerializeField]
+	private float verticalSpeed = 3f;
+
+	// Maximum vertical speed of player object
+	[SerializeField]
+	private float maxVerticalSpeed = 6.5f;
+
+	// Downwards pull modifier
+	[SerializeField]
+	private float gravity = 2f;
+
+	// Jump modifier
+	[SerializeField]
+	private float jumpModifier = 8f;
+
+    void Start()
+	{
+		// Get component and store it in the field variable
+		rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+		// Move player right every fame
+		if (rigidBody.velocity.x < maxVerticalSpeed)
+		{
+			rigidBody.AddForce(new Vector2(verticalSpeed, 0f), ForceMode2D.Force);
+		}
+		
+		// When player presses space, jump
+		if (Input.GetKeyDown(KeyCode.Space) && rigidBody.velocity.y == 0)
+		{
+			rigidBody.AddForce(new Vector2(0f, jumpModifier), ForceMode2D.Impulse);
+		}
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject prefabStart;
+	[SerializeField]
+	private GameObject prefabOne;
+
+	private Transform gridPosition;
+
+	void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			print($"Hit {collision.gameObject.name}");
+
+			Instantiate(prefabOne, transform.parent.position + new Vector3(40, 0, 0), Quaternion.identity);
+		}
+	}
+}
