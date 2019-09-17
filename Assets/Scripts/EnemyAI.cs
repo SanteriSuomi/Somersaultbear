a8137ee3b4229f4b1bbeb736d1471bf794d3247d
Santeri Suomi <sant.suomi@gmail.com>
2019-09-17 15:30:47 +0300

    Enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+	[SerializeField]
+	private float verticalSpeed = 4f;
+
+	[SerializeField]
+	private float hitDetectionDistance = 1f;
+
+	// If one direction == true, when the enemy spawns it will just charge in one direction.
+	[SerializeField]
+	private bool oneDirection = false;
+
+	// If true, enable OnEnable addforce.
+	[SerializeField]
+	private bool onEnable = true;
+
+	// Starting speed OnEnable/On Start.
+	[SerializeField]
+	private float startSpeed = 2f;
+
+	// Starting direction, X 1/-1.
+	[SerializeField]
+	private Vector2 startDirection = Vector2.left;
+
+	// Layer to use raycast with.
+	[SerializeField]
+	private LayerMask groundLayer = default;
+
+	private Rigidbody2D rigidBody = default;
+
+	void Start()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+		
+		// If onEnable is false, activate speed at the start of the game.
+		if (!onEnable)
+		{
+			rigidBody.AddForce(startDirection * startSpeed, ForceMode2D.Impulse);
+		}
+	}
+
+	void OnEnable()
+	{
+		// If onEnable true, activate speed when the prefab gets enabled/activated.
+		if (onEnable)
+		{
+			rigidBody.AddForce(startDirection * startSpeed, ForceMode2D.Impulse);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		// If oneDirection is false, cast 2 rays on left and right and move to the left and right according to collisions.
+		if (!oneDirection)
+		{
+			RaycastHit2D hitXPositive = Physics2D.Raycast(transform.position, Vector2.right, hitDetectionDistance, groundLayer);
+
+			Debug.DrawRay(transform.position, Vector2.right * hitDetectionDistance, Color.white);
+
+			RaycastHit2D hitXNegative = Physics2D.Raycast(transform.position, Vector2.left, hitDetectionDistance, groundLayer);
+
+			Debug.DrawRay(transform.position, Vector2.left * hitDetectionDistance, Color.white);
+
+			if (hitXPositive && hitXPositive.collider)
+			{
+				rigidBody.AddForce(Vector2.left * verticalSpeed, ForceMode2D.Impulse);
+			}
+			else if (hitXNegative && hitXNegative.collider)
+			{
+				rigidBody.AddForce(Vector2.right * verticalSpeed, ForceMode2D.Impulse);
+			}
+		}
+	}
+}
