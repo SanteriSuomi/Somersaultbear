a08d3d790c2ec3b406fb9f204c5fa707f4d6add9
SanteriSuomi <sant.suomi@gmail.com>
2019-09-25 13:12:54 +0300

    Scrolling background WIP

--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -14,24 +14,26 @@ public class ScrollingBackground : MonoBehaviour
     private Vector2 backgroundNewPosition;
 
     [SerializeField]
-    private float backgroundSpeed = 0.05f;
+    private float offsetSmooth = 1f;
 
     [SerializeField]
-    private float offsetSmooth = 1f;
+    private float unitsToOffset = 1f;
 
     private float backgroundRendererOffsetX;
 
-    private float zeroVelocity = Mathf.Epsilon;
+    private float currentMaxX;
 
     private float yVelocity = 0;
 
-    private const float UNITS_TO_OFFSET = 1f;
+    private const float MIN_X_VELOCITY = 0.05f;
 
     private void Start()
     {
         characterRigidbody = character.GetComponent<Rigidbody2D>();
 
         backgroundRenderer = GetComponent<MeshRenderer>();
+
+        currentMaxX = character.transform.position.x;
 
         #if UNITY_EDITOR
         Assert.IsNotNull(character);
@@ -41,30 +43,36 @@ public class ScrollingBackground : MonoBehaviour
 
     private void Update()
     {
+        if (character.transform.position.x > currentMaxX)
+        {
+            currentMaxX = character.transform.position.x;
+        }
+
         // Update the current material offset X to the field.
         backgroundRendererOffsetX = backgroundRenderer.material.mainTextureOffset.x;
 
         // If the instance isn't the menu background.
-        if (characterRigidbody.velocity.x > zeroVelocity)
+        if (characterRigidbody.velocity.x > MIN_X_VELOCITY)
         {
             // Move the repeating texture on the quad on X axis to create a scrolling background effect.
             MoveOffsetRight();
         }
-        else if (characterRigidbody.velocity.x < zeroVelocity)
+        else if (characterRigidbody.velocity.x < -MIN_X_VELOCITY)
         {
             MoveOffsetLeft();
         }
 
+        // Update the texture offset after the method calls.
         backgroundRenderer.material.mainTextureOffset = backgroundNewPosition;
     }
 
     private void MoveOffsetRight()
     {
-        backgroundNewPosition = new Vector2(Mathf.SmoothDamp(backgroundRendererOffsetX, backgroundRendererOffsetX + UNITS_TO_OFFSET, ref yVelocity, offsetSmooth), 0);
+        backgroundNewPosition = new Vector2(Mathf.SmoothDamp(backgroundRendererOffsetX, backgroundRendererOffsetX + unitsToOffset, ref yVelocity, offsetSmooth), 0);
     }
 
     private void MoveOffsetLeft()
     {
-        backgroundNewPosition = new Vector2(Mathf.SmoothDamp(backgroundRendererOffsetX, backgroundRendererOffsetX - UNITS_TO_OFFSET, ref yVelocity, offsetSmooth), 0);
+        backgroundNewPosition = new Vector2(Mathf.SmoothDamp(backgroundRendererOffsetX, backgroundRendererOffsetX - unitsToOffset, ref yVelocity, offsetSmooth), 0);
     }
 }
