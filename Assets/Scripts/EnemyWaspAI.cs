02b02f062f3f3f5c74dd817f2aa65dbf15b718c9
SanteriSuomi <sant.suomi@gmail.com>
2019-09-25 15:48:08 +0300

    no message

---
+++ Assets/Scripts/EnemyWaspAI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyWaspAI : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+    private Vector3 target;
+
+    private bool isInArea = false;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();    
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            target = other.transform.position;
+
+            Vector3.Lerp(transform.position, target, 0.5f);
+        }
+        else
+        {
+
+        }
+    }
+
+    void Update()
+    {
+        /*
+        if (isInArea)
+        {
+            Vector3.Lerp(transform.position, target, 0.5f);
+        }
+        */
+    }
+}
