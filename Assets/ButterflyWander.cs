a59fc14939bafe75f54eb3d4d071f28478539f7f
Santeri Suomi <sant.suomi@gmail.com>
2019-09-26 21:25:36 +0300

    Butterfly wander 1.0

--- Assets/ButterflyWander.cs
+++ Assets/ButterflyWander.cs
@@ -3,18 +3,71 @@ using UnityEngine;
 
 public class ButterflyWander : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] points;
+    SpriteRenderer spriteRenderer;
+
     [SerializeField]
     private float moveSpeed = 5f;
+    [SerializeField]
+    private float radius = 2.5f;
+
+    private float distance;
+
+    Vector2 target;
+
+    private enum State
+    {
+        Wander,
+        Stop
+    }
+
+    private State currentState;
 
     private void OnEnable()
     {
-        transform.position = points[0].transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        GetNewPoint();
     }
 
     private void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, points[1].transform.position, moveSpeed * Time.deltaTime);
+        switch (currentState)
+        {
+            case State.Wander:
+                StartCoroutine(Move());
+                break;
+            case State.Stop:
+                break;
+        }
+        if (target.x > transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private IEnumerator Move()
+    {
+        distance = Vector2.Distance(transform.position, target);
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        yield return new WaitUntil(() => distance <= 0.2f);
+        GetNewPoint();
+    }
+
+    private void GetNewPoint()
+    {
+        target = Random.insideUnitCircle * radius;
+    }
+
+    private void ChangeState(State state)
+    {
+        currentState = state;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, target);
     }
 }
--- Assets/Scripts/EnemyWaspAI.cs
+++ Assets/Scripts/EnemyWaspAI.cs
@@ -13,6 +13,7 @@ public class EnemyWaspAI : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 1f;
 
+
     private void OnEnable()
     {
         HitPoints = 3;
