60d8bdb24d346aea11f8d2e41a5052a180734814
Santeri Suomi <sant.suomi@gmail.com>
2019-09-27 10:46:17 +0300

    Raccoon anims WIP

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private LayerMask groundLayer = default;
     private Rigidbody2D rigidBody = default;
     private AudioSource audioSource = default;
+    private Animator animator = default;
 
     [SerializeField]
     private float verticalSpeed = 3f;
@@ -19,12 +20,14 @@ public class PlayerMovement : MonoBehaviour
 
     private bool pressedSpace = false;
 
+    // Prevent double jumping with small velocity check.
     private const float RB_Y_VELOCITY_MAX = 0.5f;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -39,6 +42,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rigidBody.AddForce(Vector2.right * verticalSpeed, ForceMode2D.Force);
         }
+
         RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.down, jumpDetectionHeight, groundLayer);
 
         #if UNITY_EDITOR
@@ -49,6 +53,24 @@ public class PlayerMovement : MonoBehaviour
         {
             rigidBody.AddForce(Vector2.up * jumpModifier, ForceMode2D.Impulse);
             audioSource.Play();
+        }
+
+        // Play animations.
+        PlayAnimJump();
+    }
+
+    private void PlayAnimJump()
+    {
+        if (rigidBody.velocity.y > 1f)
+        {
+            animator.SetBool("isJumping", true);
+            rigidBody.freezeRotation = true;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+            rigidBody.freezeRotation = false;
         }
     }
 }
